5697d8b09a7a7583b99fadb724a23a784a7f5537
Bhagyatgn <nethmibhagya5131@gmail.com>
2026-07-24 16:46:16 +0530

    feat(web): improve application layout and routing

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,5 +1,10 @@
 import { AppRoutes } from "./routes";
+import { Providers } from "./providers";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <Providers>
+      <AppRoutes />
+    </Providers>
+  );
 }
