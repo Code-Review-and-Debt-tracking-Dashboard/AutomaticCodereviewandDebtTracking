3f2cb97a0199b541a3991b89ed013166f4b17a29
RumeshChathuranga <Rumarumesh1220@gmail.com>
2026-09-13 00:34:24 +0530

    feat: debt score B-14

--- apps/worker/src/processors/analysisProcessor.ts
+++ apps/worker/src/processors/analysisProcessor.ts
@@ -161,7 +161,12 @@ export async function analysisProcessor(job: Job<AnalysisJobData>) {
     });
 
     logger.info(
-      { analysisId, healthScore: score.healthScore, ...score.penaltyBreakdown },
+      {
+        analysisId,
+        healthScore: score.healthScore,
+        debtMinutes: score.debtMinutes,
+        ...score.penaltyBreakdown,
+      },
       'Score computed',
     );
 
